1b043c3d2e45ff865479a53ab5bd7f0b029f9d06
Tran Duy Tuong <duytuong0503@gmail.com>
2023-07-23 20:03:40 +0700

    upload function show modal import file

--- application/KIKANSYSTEM/SOURCE/TXTKikanSystem/TXTKikanSystem/wwwroot/js/Books.js
+++ application/KIKANSYSTEM/SOURCE/TXTKikanSystem/TXTKikanSystem/wwwroot/js/Books.js
@@ -1,6 +1,7 @@
 ﻿import { GetCookies } from '../Common/CommonSetting.js'
 import { UserLogin } from '../Common/CommonDataDefault.js'
 import { HomePage } from '../Common/CommonUrl.js'
+import { ModalImportFile } from '../Common/CommonLoading.js'
 $(document).ready(function () {
 
     // Reditrect Home Page
@@ -10,6 +11,12 @@ $(document).ready(function () {
         var queryString = location.origin;
         var stringData = localStorage.getItem("UserID") + "*" + localStorage.getItem("RoleEmployer") + "*" + cookiesname;
         window.location.href = queryString + "/" + HomePage + "?Carshier=" + stringData;
-    })
+    });
+
+    // Show dialog Import File
+    $("#btn_ShowDialogFile").click(function () {
+        $("#ImportFile").append(ModalImportFile("Import Books File"));
+        $("#dialogImport").show();
+    });
 
 })
