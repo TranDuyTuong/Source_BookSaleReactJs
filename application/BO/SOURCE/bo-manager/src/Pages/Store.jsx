226e46e97b236c0237b7e5c63b063a30a098dc6a
Tuong.td <tuong.td@vn.vinx.asia>
2023-08-24 17:50:05 +0700

    update function create store

--- application/BO/SOURCE/bo-manager/src/Pages/Store.jsx
+++ application/BO/SOURCE/bo-manager/src/Pages/Store.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "../Styles/Home.css";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
@@ -18,7 +18,7 @@ import {
   faStore,
   faEye,
 } from "@fortawesome/free-solid-svg-icons";
-import "../Styles/Area.css";
+import "../Styles/Store.css";
 import { HandleSeachStore } from "../ApiLablary/StoreApi";
 import { GetCookies, ConcatStringEvent } from "../ObjectCommon/FunctionCommon";
 import { UserLogin } from "../ObjectCommon/EventCommon";
@@ -26,14 +26,13 @@ import {
   CompanyCode,
   FistCode,
   EventSeachStore,
-  EventConfirmArea,
 } from "../ObjectCommon/EventCommon";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import { StoreReducer } from "../ReduxCommon/ReducerCommon/ReducerStore";
 import { Create, Update, Delete, Revert } from "../Contants/DataContant";
-import LoadingModal from "../CommonPage/LoadingCommon";
 import moment from "moment";
+import { titleCreate } from "../MessageCommon/Message";
 
 // Main Function
 function Store() {
@@ -41,17 +40,41 @@ function Store() {
   const dispatch = useDispatch();
   // Call list area in Redux
   const listStoreResult = useSelector((item) => item.storeData.ListStore);
+  // TypeOf Dialog Setting
+  const [typeOfDialog, setTypeOfDialog] = useState();
+  // Show And Hide Dialog Setting
+  const [show, setShow] = useState(false);
+  // Title Dialog
+  const [titleDialog, setTitleDialog] = useState("");
 
   // State Seach Store
   const [seachStore, setSeachStore] = useState("");
   const [messageError, setMessageError] = useState("");
+  // State store
+  const [state_StoreCode, setStoreCode] = useState("");
+  const [state_Description, setDescription] = useState("");
+  const [state_DateCreate, setDateCreate] = useState("");
+  const [state_Address, setAddress] = useState("");
+
+  // Message validation Form when Submit, Create - Update - Delete - Revert
+  const [messageErrorForm, setMessageErrorForm] = useState("");
+
+  // Forcus Element
+  const ref_StoreCode = useRef(null);
 
   useEffect(() => {
     // Setting Title Page
     document.title = "Store";
-    var datess = new Date("2023-08-20T16:20:29.2938759");
-    alert(moment(datess).format("YYYY-MM-DD"));
   }, []);
+
+  // Focus item create, update, delete
+  useEffect(() => {
+    if (show === true) {
+      if (typeOfDialog === Create) {
+        ref_StoreCode.current.focus();
+      }
+    }
+  }, typeOfDialog);
 
   // Handle Seach Store
   const HandleSeachStoreUI = async (e) => {
@@ -78,14 +101,45 @@ function Store() {
       // Seach Fail
       setMessageError(result.MessageError);
     } else {
-      result.ListStore.forEach(function (fruit) {
-        alert(fruit.DateCreate); // Apple, Orange, Banna, Mango
+      const listStore = [];
+
+      result.ListStore.forEach(function (item) {
+        const store = {
+          StoreCode: item.StoreCode,
+          Description: item.Description,
+          DateCreate: moment(item.DateCreate).format("YYYY-MM-DD"),
+          LastUpdateDate:
+            item.LastUpdateDate === null
+              ? "--"
+              : moment(item.LastUpdateDate).format("YYYY-MM-DD"),
+          Address: item.Address,
+          TypeOf: null,
+          OldType: null,
+        };
+        listStore.push(store);
       });
       // Save Store List In To Redux
-      //dispatch(StoreReducer.actions.SeachStore(result.ListStore));
+      dispatch(StoreReducer.actions.SeachStore(listStore));
       // Seach Success
       setMessageError(result.MessageError);
     }
+  };
+
+  // Handle Close Dialog Setting
+  const HandleCloseDialogUI = (e) => {
+    setTypeOfDialog(null);
+    setShow(false);
+  };
+
+  // Handle Add Store
+  const HandleCreateStoreUI = (e) => {
+    setTypeOfDialog(Create);
+    setShow(true);
+    setTitleDialog(titleCreate);
+    setStoreCode("");
+    setDescription("");
+    setDateCreate("");
+    setAddress("");
   };
 
   return (
@@ -112,7 +166,11 @@ function Store() {
               </Form.Group>
               <Form.Group as={Col} md="7" controlId="validationCustom02">
                 <p className="btnMain">
-                  <Button variant="primary" className="btnOption">
+                  <Button
+                    variant="primary"
+                    className="btnOption"
+                    onClick={() => HandleCreateStoreUI()}
+                  >
                     <FontAwesomeIcon icon={faPlusSquare} /> Add
                   </Button>
                   <Button
@@ -148,12 +206,14 @@ function Store() {
                   (item) =>
                     (item.TypeOf === Create && (
                       <tr key={item.StoreCode}>
-                        <td style={{ color: "blue" }}>{item.StoreCode}</td>
+                        <td className="firsColum" style={{ color: "blue" }}>
+                          {item.StoreCode}
+                        </td>
                         <td style={{ color: "blue" }}>{item.Description}</td>
                         <td style={{ color: "blue" }}>{item.DateCreate}</td>
                         <td style={{ color: "blue" }}>{item.LastUpdateDate}</td>
                         <td style={{ color: "blue" }}>{Create}</td>
-                        <td>
+                        <td className="lastColum">
                           <Button variant="warning" className="btnOption">
                             <FontAwesomeIcon icon={faEdit} /> Update
                           </Button>
@@ -168,12 +228,14 @@ function Store() {
                     )) ||
                     (item.TypeOf === Update && (
                       <tr key={item.StoreCode}>
-                        <td style={{ color: "red" }}>{item.StoreCode}</td>
+                        <td className="firsColum" style={{ color: "red" }}>
+                          {item.StoreCode}
+                        </td>
                         <td style={{ color: "red" }}>{item.Description}</td>
                         <td style={{ color: "red" }}>{item.DateCreate}</td>
                         <td style={{ color: "red" }}>{item.LastUpdateDate}</td>
                         <td style={{ color: "red" }}>{Update}</td>
-                        <td>
+                        <td className="lastColum">
                           <Button variant="warning" className="btnOption">
                             <FontAwesomeIcon icon={faEdit} /> Update
                           </Button>
@@ -188,12 +250,12 @@ function Store() {
                     )) ||
                     (item.TypeOf === Delete && (
                       <tr key={item.StoreCode} className="trChildItem">
-                        <td>{item.StoreCode}</td>
+                        <td className="firsColum">{item.StoreCode}</td>
                         <td>{item.Description}</td>
                         <td>{item.DateCreate}</td>
                         <td>{item.LastUpdateDate}</td>
                         <td>{Delete}</td>
-                        <td>
+                        <td className="lastColum">
                           <Button variant="secondary" className="btnOption">
                             <FontAwesomeIcon icon={faClockRotateLeft} /> Revert
                           </Button>
@@ -205,12 +267,12 @@ function Store() {
                     )) ||
                     (item.TypeOf === null && (
                       <tr key={item.StoreCode}>
-                        <td>{item.StoreCode}</td>
+                        <td className="firsColum">{item.StoreCode}</td>
                         <td>{item.Description}</td>
                         <td>{item.DateCreate}</td>
                         <td>{item.LastUpdateDate}</td>
                         <td></td>
-                        <td>
+                        <td className="lastColum">
                           <Button variant="warning" className="btnOption">
                             <FontAwesomeIcon icon={faEdit} /> Update
                           </Button>
@@ -230,6 +292,70 @@ function Store() {
         </Col>
       </Row>
       {/* DiaLog show Add, update, delete, Confirm */}
+      <Modal show={show}>
+        <Modal.Header className="settingBackround">
+          <Modal.Title>{titleDialog}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="settingBackround">
+          {typeOfDialog === Create && (
+            <div>
+              <p className="errorMessage">{messageErrorForm}</p>
+              <Form.Group as={Col} md="12">
+                <Form.Label className="labelForm">
+                  StoreCode <span className="requestData">*</span>
+                </Form.Label>
+                <Form.Control
+                  ref={ref_StoreCode}
+                  type="text"
+                  value={state_StoreCode}
+                  id="txtFocusStoreCode"
+                  placeholder="Enter StoreCode..."
+                  onChange={(e) => setStoreCode(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Group as={Col} md="12">
+                <Form.Label className="labelForm">Description *</Form.Label>
+                <Form.Control
+                  value={state_Description}
+                  type="text"
+                  id="txtFocusDescription"
+                  placeholder="Enter Description..."
+                  onChange={(e) => setDescription(e.target.value)}
+                />
+              </Form.Group>
+
+              <Form.Group as={Col} md="12">
+                <Form.Label className="labelForm">DateCreate *</Form.Label>
+                <Form.Control
+                  type="date"
+                  id="txtFocusDateCreate"
+                  value={state_DateCreate}
+                  onChange={(e) => setDateCreate(e.target.value)}
+                />
+              </Form.Group>
+
+              <Form.Group as={Col} md="12">
+                <Form.Label className="labelForm">Address *</Form.Label>
+                <Form.Control
+                  value={state_Address}
+                  type="text"
+                  id="txtFocusAddress"
+                  placeholder="Enter Address..."
+                  onChange={(e) => setAddress(e.target.value)}
+                />
+              </Form.Group>
+            </div>
+          )}
+        </Modal.Body>
+        <Modal.Footer className="settingBackround">
+          <Button variant="secondary" onClick={() => HandleCloseDialogUI()}>
+            Close
+          </Button>
+          <Button variant="primary">
+            <FontAwesomeIcon icon={faPlusSquare} /> Ok
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </Container>
   );
 }
